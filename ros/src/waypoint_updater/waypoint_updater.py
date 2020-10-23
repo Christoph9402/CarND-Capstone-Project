34532ef27c4e31aad6380a5295d384ff981b1284
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-10-23 13:23:06 +0200

    walkthrough implemented

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -34,7 +34,7 @@ class WaypointUpdater(object):
         self.base_lane = None
         self.waypoints_2d = None
         self.waypoint_tree = None
-        self.stopline_wp_idx = None
+        self.stopline_wp_idx = -1
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -94,7 +94,7 @@ class WaypointUpdater(object):
             p=Waypoint()
             p.pose=wp.pose
 
-            stop_idx=max(self.stopline_wp_idx - closest_idx - 2,0)
+            stop_idx=max(self.stopline_wp_idx - closest_idx - 2, 0)
             dist=self.distance(waypoints,i,stop_idx)
             vel=math.sqrt(2*MAX_DECEL*dist)
             if vel < 1.:
