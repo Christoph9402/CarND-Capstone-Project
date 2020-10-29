77b5504d68a016969c7697ad0708ad3fe99ec765
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-10-29 12:36:25 +0100

    test

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -74,7 +74,7 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
-
+        rospy.rate(10)
         self.has_image = True
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
