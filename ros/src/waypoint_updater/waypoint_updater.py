4f79b4dde54fc9d2349715acc1ded5ffdf863974
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-11-09 15:24:37 +0100

    test

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -28,16 +28,13 @@ MAX_DECEL = .5
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater',log_level=rospy.DEBUG)
+        rospy.init_node('waypoint_updater')
         # TODO: Add other member variables you need below
         self.pose = None
         self.base_lane = None
         self.waypoints_2d = None
         self.waypoint_tree = None
         self.stopline_wp_idx = -1
-        rospy.loginfo("test1")
-        print("test2")
-        rospy.loginfo('test3')
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
         rospy.Subscriber('/traffic_waypoint',Int32,self.traffic_cb)
@@ -50,7 +47,7 @@ class WaypointUpdater(object):
 
     def loop(self):
 
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(25)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 self.publish_waypoints()
@@ -59,7 +56,8 @@ class WaypointUpdater(object):
     def get_closest_waypoint_idx(self):
         x=self.pose.pose.position.x
         y=self.pose.pose.position.y
-        closest_idx = self.waypoint_tree.query([x,y],1)[1]
+        if (self.pose and self.waypoint_tree):
+            closest_idx = self.waypoint_tree.query([x,y],1)[1]
 
         closest_coord=self.waypoints_2d[closest_idx]
         prev_coord=self.waypoints_2d[closest_idx-1]
