b1039fc1b26f605abcb2018cd2c28ff85c2c06c0
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-11-09 20:28:01 +0100

    test

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -47,7 +47,7 @@ class WaypointUpdater(object):
 
     def loop(self):
 
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:# and self.waypoint_tree:
                 self.publish_waypoints()
