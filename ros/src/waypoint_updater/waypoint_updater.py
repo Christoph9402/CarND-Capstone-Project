97cba635b8556c1a68c890889e141505461fd9d8
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-11-09 21:54:30 +0100

    test

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
 MAX_DECEL = .5
 
 class WaypointUpdater(object):
@@ -47,7 +47,7 @@ class WaypointUpdater(object):
 
     def loop(self):
 
-        rate = rospy.Rate(15)
+        rate = rospy.Rate(5)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:# and self.waypoint_tree:
                 self.publish_waypoints()
