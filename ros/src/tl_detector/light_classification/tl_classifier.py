dd52328012f3af982cb267aa8ab1255a71fb7e14
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-11-15 01:51:05 +0100

    test

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -59,3 +59,14 @@ class TLClassifier(object):
 
 
         #return TrafficLight.UNKNOWN
+
+    def load_graph(graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
