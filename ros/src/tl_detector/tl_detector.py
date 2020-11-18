41110d0cae0c3aa3622d3cbceabadb056f4b0437
Christoph9402 <62546080+Christoph9402@users.noreply.github.com>
2020-11-18 10:51:27 +0100

    test

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -176,15 +176,15 @@ class TLDetector(object):
 
         """
 
-        return light.state
-        #if(not self.has_image):
-        #    self.prev_light_loc = None
-        #    return False
+        #return light.state
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        #return self.light_classifier.get_classification(cv_image)
+        return self.light_classifier.get_classification(cv_image)
 
 
 
